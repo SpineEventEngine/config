5ef0d1231738714297924e5e50c8c480838a7990
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2020-05-06 14:58:13 +0300

    Fix spelling

--- gradle/dependencies.gradle.kts
+++ gradle/dependencies.gradle.kts
@@ -95,91 +95,91 @@ object Versions {
 }
 
 object GradlePlugins {
-    val errorProne      = "net.ltgt.gradle:gradle-errorprone-plugin:${versions.errorPronePlugin}"
-    val protobuf        = "com.google.protobuf:protobuf-gradle-plugin:${versions.protobufPlugin}"
-    val appengine       = "com.google.cloud.tools:appengine-gradle-plugin:${versions.appenginePlugin}"
-    val licenseReport   = "com.github.jk1:gradle-license-report:${versions.licensePlugin}"
+    val errorProne      = "net.ltgt.gradle:gradle-errorprone-plugin:${Versions.errorPronePlugin}"
+    val protobuf        = "com.google.protobuf:protobuf-gradle-plugin:${Versions.protobufPlugin}"
+    val appengine       = "com.google.cloud.tools:appengine-gradle-plugin:${Versions.appenginePlugin}"
+    val licenseReport   = "com.github.jk1:gradle-license-report:${Versions.licensePlugin}"
 }
 
 object AutoService {
-    val annotations = "com.google.auto.service:auto-service-annotations:${versions.autoService}"
-    val processor = "com.google.auto.service:auto-service:${versions.autoService}"
+    val annotations = "com.google.auto.service:auto-service-annotations:${Versions.autoService}"
+    val processor = "com.google.auto.service:auto-service:${Versions.autoService}"
 }
 
 object Build {
-    val errorProneJavac        = "com.google.errorprone:javac:${versions.errorProneJavac}"
+    val errorProneJavac        = "com.google.errorprone:javac:${Versions.errorProneJavac}"
     val errorProneAnnotations = listOf(
-            "com.google.errorprone:error_prone_annotations:${versions.errorProne}",
-            "com.google.errorprone:error_prone_type_annotations:${versions.errorProne}"
+            "com.google.errorprone:error_prone_annotations:${Versions.errorProne}",
+            "com.google.errorprone:error_prone_type_annotations:${Versions.errorProne}"
     )
-    val errorProneCheckApi     = "com.google.errorprone:error_prone_check_api:${versions.errorProne}"
-    val errorProneCore         = "com.google.errorprone:error_prone_core:${versions.errorProne}"
-    val errorProneTestHelpers  = "com.google.errorprone:error_prone_test_helpers:${versions.errorProne}"
-    val checkerAnnotations     = "org.checkerframework:checker-qual:${versions.checkerFramework}"
+    val errorProneCheckApi     = "com.google.errorprone:error_prone_check_api:${Versions.errorProne}"
+    val errorProneCore         = "com.google.errorprone:error_prone_core:${Versions.errorProne}"
+    val errorProneTestHelpers  = "com.google.errorprone:error_prone_test_helpers:${Versions.errorProne}"
+    val checkerAnnotations     = "org.checkerframework:checker-qual:${Versions.checkerFramework}"
     val checkerDataflow        = listOf(
-            "org.checkerframework:dataflow:${versions.checkerFramework}",
-            "org.checkerframework:javacutil:${versions.checkerFramework}"
+            "org.checkerframework:dataflow:${Versions.checkerFramework}",
+            "org.checkerframework:javacutil:${Versions.checkerFramework}"
     )
-    val autoCommon             = "com.google.auto:auto-common:${versions.autoCommon}"
+    val autoCommon             = "com.google.auto:auto-common:${Versions.autoCommon}"
     val autoService = AutoService
-    val jsr305Annotations      = "com.google.code.findbugs:jsr305:${versions.findBugs}"
-    val guava                  = "com.google.guava:guava:${versions.guava}"
-    val flogger                = "com.google.flogger:flogger:${versions.flogger}"
-    val slf4j                  = "org.slf4j:slf4j-api:${versions.slf4j}"
+    val jsr305Annotations      = "com.google.code.findbugs:jsr305:${Versions.findBugs}"
+    val guava                  = "com.google.guava:guava:${Versions.guava}"
+    val flogger                = "com.google.flogger:flogger:${Versions.flogger}"
+    val slf4j                  = "org.slf4j:slf4j-api:${Versions.slf4j}"
     val protobuf = listOf(
-            "com.google.protobuf:protobuf-java:${versions.protobuf}",
-            "com.google.protobuf:protobuf-java-util:${versions.protobuf}"
+            "com.google.protobuf:protobuf-java:${Versions.protobuf}",
+            "com.google.protobuf:protobuf-java-util:${Versions.protobuf}"
     )
-    val protoc                 = "com.google.protobuf:protoc:${versions.protobuf}"
-    val googleHttpClient       = "com.google.http-client:google-http-client:${versions.httpClient}"
-    val googleHttpClientApache = "com.google.http-client:google-http-client-apache:${versions.apacheHttpClient}"
-    val appengineApi           = "com.google.appengine:appengine-api-1.0-sdk:${versions.appengineApi}"
-    val firebaseAdmin          = "com.google.firebase:firebase-admin:${versions.firebaseAdmin}"
-    val jacksonDatabind        = "com.fasterxml.jackson.core:jackson-databind:${versions.jackson}"
-    val roasterApi             = "org.jboss.forge.roaster:roaster-api:${versions.roaster}"
-    val roasterJdt             = "org.jboss.forge.roaster:roaster-jdt:${versions.roaster}"
-    val animalSniffer          = "org.codehaus.mojo:animal-sniffer-annotations:${versions.animalSniffer}"
+    val protoc                 = "com.google.protobuf:protoc:${Versions.protobuf}"
+    val googleHttpClient       = "com.google.http-client:google-http-client:${Versions.httpClient}"
+    val googleHttpClientApache = "com.google.http-client:google-http-client-apache:${Versions.apacheHttpClient}"
+    val appengineApi           = "com.google.appengine:appengine-api-1.0-sdk:${Versions.appengineApi}"
+    val firebaseAdmin          = "com.google.firebase:firebase-admin:${Versions.firebaseAdmin}"
+    val jacksonDatabind        = "com.fasterxml.jackson.core:jackson-databind:${Versions.jackson}"
+    val roasterApi             = "org.jboss.forge.roaster:roaster-api:${Versions.roaster}"
+    val roasterJdt             = "org.jboss.forge.roaster:roaster-jdt:${Versions.roaster}"
+    val animalSniffer          = "org.codehaus.mojo:animal-sniffer-annotations:${Versions.animalSniffer}"
     val ci = "true".equals(System.getenv("CI"))
     val gradlePlugins = GradlePlugins
 }
 
 object Gen {
-    val javaPoet = "com.squareup:javapoet:${versions.javaPoet}"
+    val javaPoet = "com.squareup:javapoet:${Versions.javaPoet}"
 }
 
 object Grpc {
-    val grpcCore               = "io.grpc:grpc-core:${versions.grpc}"
-    val grpcStub               = "io.grpc:grpc-stub:${versions.grpc}"
-    val grpcOkHttp             = "io.grpc:grpc-okhttp:${versions.grpc}"
-    val grpcProtobuf           = "io.grpc:grpc-protobuf:${versions.grpc}"
-    val grpcNetty              = "io.grpc:grpc-netty:${versions.grpc}"
-    val grpcNettyShaded        = "io.grpc:grpc-netty-shaded:${versions.grpc}"
-    val grpcContext            = "io.grpc:grpc-context:${versions.grpc}"
+    val grpcCore               = "io.grpc:grpc-core:${Versions.grpc}"
+    val grpcStub               = "io.grpc:grpc-stub:${Versions.grpc}"
+    val grpcOkHttp             = "io.grpc:grpc-okhttp:${Versions.grpc}"
+    val grpcProtobuf           = "io.grpc:grpc-protobuf:${Versions.grpc}"
+    val grpcNetty              = "io.grpc:grpc-netty:${Versions.grpc}"
+    val grpcNettyShaded        = "io.grpc:grpc-netty-shaded:${Versions.grpc}"
+    val grpcContext            = "io.grpc:grpc-context:${Versions.grpc}"
 }
 
 object Runtime {
-    val floggerSystemBackend = "com.google.flogger:flogger-system-backend:${versions.flogger}"
-    val floggerLog4J         = "com.google.flogger:flogger-log4j:${versions.flogger}"
-    val floggerSlf4J         = "com.google.flogger:slf4j-backend-factory:${versions.flogger}"
+    val floggerSystemBackend = "com.google.flogger:flogger-system-backend:${Versions.flogger}"
+    val floggerLog4J         = "com.google.flogger:flogger-log4j:${Versions.flogger}"
+    val floggerSlf4J         = "com.google.flogger:slf4j-backend-factory:${Versions.flogger}"
 }
 
 object Test {
-    val junit4        = "junit:junit:${versions.junit4}",
+    val junit4        = "junit:junit:${Versions.junit4}",
     val junit5Api = listOf(
-            "org.junit.jupiter:junit-jupiter-api:${versions.junit5}",
-            "org.junit.jupiter:junit-jupiter-params:${versions.junit5}",
-            "org.apiguardian:apiguardian-api:${versions.apiguardian}"
+            "org.junit.jupiter:junit-jupiter-api:${Versions.junit5}",
+            "org.junit.jupiter:junit-jupiter-params:${Versions.junit5}",
+            "org.apiguardian:apiguardian-api:${Versions.apiguardian}"
     ),
-    val junit5Runner  = "org.junit.jupiter:junit-jupiter-engine:${versions.junit5}",
-    val junitPioneer  = "org.junit-pioneer:junit-pioneer:${versions.junitPioneer}",
-    val slf4j         = "org.slf4j:slf4j-jdk14:${versions.slf4j}",
-    val guavaTestlib  = "com.google.guava:guava-testlib:${versions.guava}",
+    val junit5Runner  = "org.junit.jupiter:junit-jupiter-engine:${Versions.junit5}",
+    val junitPioneer  = "org.junit-pioneer:junit-pioneer:${Versions.junitPioneer}",
+    val slf4j         = "org.slf4j:slf4j-jdk14:${Versions.slf4j}",
+    val guavaTestlib  = "com.google.guava:guava-testlib:${Versions.guava}",
     val mockito       = "org.mockito:mockito-core:2.12.0",
     val hamcrest      = "org.hamcrest:hamcrest-all:1.3",
     val truth = listOf(
-            "com.google.truth:truth:${versions.truth}",
-            "com.google.truth.extensions:truth-java8-extension:${versions.truth}",
-            "com.google.truth.extensions:truth-proto-extension:${versions.truth}"
+            "com.google.truth:truth:${Versions.truth}",
+            "com.google.truth.extensions:truth-java8-extension:${Versions.truth}",
+            "com.google.truth.extensions:truth-proto-extension:${Versions.truth}"
     )
 }
 
