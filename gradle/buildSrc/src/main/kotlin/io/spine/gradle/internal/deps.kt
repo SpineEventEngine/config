83faeaaf1983795f105eb1a9ceb574cb8b966f17
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2020-05-18 16:24:28 +0300

    Document SLF4J version

--- gradle/buildSrc/src/main/kotlin/io/spine/gradle/internal/deps.kt
+++ gradle/buildSrc/src/main/kotlin/io/spine/gradle/internal/deps.kt
@@ -67,8 +67,6 @@ object Repos {
 }
 
 object Versions {
-    @Deprecated("Use Flogger over SLF4J.", replaceWith = ReplaceWith("flogger"))
-    val slf4j            = "1.7.29"
     val checkerFramework = "3.3.0"
     val errorProne       = "2.3.4"
     val errorProneJavac  = "9+181-r4173-1" // taken from here: https://github.com/tbroyer/gradle-errorprone-plugin/blob/v0.8/build.gradle.kts
@@ -99,6 +97,18 @@ object Versions {
     val jackson          = "2.9.10.4"
     val animalSniffer    = "1.18"
     val apiguardian      = "1.1.0"
+
+    /**
+     * Version of the SLF4J library.
+     *
+     * Spine used to log with SLF4J. Now we use Flogger. Whenever a coice comes up, we recommend to
+     * use the latter.
+     *
+     * Some third-party libraries may clash with different versions of the library. Thus, we specify
+     * this version and force it via [forceConfiguration(..)][DependencyResolution.forceConfiguration].
+     */
+    @Deprecated("Use Flogger over SLF4J.", replaceWith = ReplaceWith("flogger"))
+    val slf4j            = "1.7.29"
 }
 
 object GradlePlugins {
