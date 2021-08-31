274cbb44953d3b192258bfdf486c765873907f20
Alex Tymchenko <alex.tymchenko@teamdev.com>
2021-08-31 14:12:11 +0300

    Avoid an redundant checks if `releases` and `snapshots` point to the same repository (e.g. GitHub Packages).

--- buildSrc/src/main/kotlin/io/spine/internal/gradle/CheckVersionIncrement.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/gradle/CheckVersionIncrement.kt
@@ -28,13 +28,13 @@ package io.spine.internal.gradle
 
 import com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import java.io.FileNotFoundException
+import java.net.URL
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
-import java.io.FileNotFoundException
-import java.net.URL
 
 /**
  * A task which verifies that the current version of the library has not been published to the given
@@ -58,7 +58,10 @@ open class CheckVersionIncrement : DefaultTask() {
     private fun fetchAndCheck() {
         val artifact = "${project.artifactPath()}/${MavenMetadata.FILE_NAME}"
         checkInRepo(repository.snapshots, artifact)
-        checkInRepo(repository.releases, artifact)
+
+        if (repository.releases != repository.snapshots) {
+            checkInRepo(repository.releases, artifact)
+        }
     }
 
     private fun checkInRepo(repoUrl: String, artifact: String) {
