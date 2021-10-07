4623db9822d7e56d507ba0eac7b63e4c770ef1e9
Alex Tymchenko <alex.tymchenko@teamdev.com>
2021-10-07 15:24:23 +0300

    Rename the types describing Checkstyle according to its official naming.

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/Checkstyle.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/Checkstyle.kt
@@ -24,45 +24,11 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package io.spine.internal.gradle.checkstyle
+package io.spine.internal.dependency
 
-import io.spine.internal.dependency.CheckStyle
-import org.gradle.api.Project
-import org.gradle.api.plugins.quality.Checkstyle
-import org.gradle.api.plugins.quality.CheckstyleExtension
-import org.gradle.api.plugins.quality.CheckstylePlugin
-import org.gradle.kotlin.dsl.the
-
-/**
- * Configures the CheckStyle plugin.
- *
- * Usage:
- * ```
- *      CheckStyleConfig.applyTo(project)
- * ```
- *
- * Note: the checks of the `test` sources are disabled.
- */
+// https://checkstyle.sourceforge.io/
+// See `config/gradle.checkstyle.gradle`.
 @Suppress("unused")
-object CheckStyleConfig {
-
-    /**
-     * Applies the configuration to the passed [project].
-     */
-    fun applyTo(project: Project) {
-        project.apply {
-            plugin(CheckstylePlugin::class.java)
-        }
-
-        with(project.the<CheckstyleExtension>()) {
-            toolVersion = CheckStyle.version
-            configFile = project.rootDir.resolve("config/quality/checkstyle.xml")
-        }
-
-        project.afterEvaluate {
-            // Disables checking the test sources.
-            val checkstyleTest = project.tasks.findByName("checkstyleTest") as Checkstyle
-            checkstyleTest.enabled = false
-        }
-    }
+object Checkstyle {
+    const val version = "8.29"
 }
--- buildSrc/src/main/kotlin/io/spine/internal/gradle/checkstyle/CheckstyleConfig.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/gradle/checkstyle/CheckstyleConfig.kt
@@ -24,11 +24,43 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package io.spine.internal.dependency
+package io.spine.internal.gradle.checkstyle
 
-// https://checkstyle.sourceforge.io/
-// See `config/gradle.checkstyle.gradle`.
+import org.gradle.api.Project
+import org.gradle.api.plugins.quality.CheckstyleExtension
+import org.gradle.api.plugins.quality.CheckstylePlugin
+import org.gradle.kotlin.dsl.the
+
+/**
+ * Configures the CheckStyle plugin.
+ *
+ * Usage:
+ * ```
+ *      CheckstyleConfig.applyTo(project)
+ * ```
+ *
+ * Note: the checks of the `test` sources are disabled.
+ */
 @Suppress("unused")
-object CheckStyle {
-    const val version = "8.29"
+object CheckstyleConfig {
+
+    /**
+     * Applies the configuration to the passed [project].
+     */
+    fun applyTo(project: Project) {
+        project.apply {
+            plugin(CheckstylePlugin::class.java)
+        }
+
+        with(project.the<CheckstyleExtension>()) {
+            toolVersion = Checkstyle.version
+            configFile = project.rootDir.resolve("config/quality/checkstyle.xml")
+        }
+
+        project.afterEvaluate {
+            // Disables checking the test sources.
+            val checkstyleTest = project.tasks.findByName("checkstyleTest") as Checkstyle
+            checkstyleTest.enabled = false
+        }
+    }
 }
