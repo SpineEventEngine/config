3a3b0d918b0c369ddf15df18433454407d2cde9d
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2021-12-01 18:57:16 +0200

    Use `Int` for JVM version

--- buildSrc/src/main/kotlin/io/spine/internal/gradle/kotlin/KotlinConfig.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/gradle/kotlin/KotlinConfig.kt
@@ -33,7 +33,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
  * Sets [Java toolchain](https://kotlinlang.org/docs/gradle.html#gradle-java-toolchains-support)
  * to the specified version (e.g. "11" or "8").
  */
-fun KotlinJvmProjectExtension.applyJvmToolchain(version: String) {
+fun KotlinJvmProjectExtension.applyJvmToolchain(version: Int) {
     jvmToolchain {
         (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(version))
     }
