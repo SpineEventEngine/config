8b8859785be8536dd73f17099333a42f18d72a97
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2021-12-01 18:43:29 +0200

    Add extension for JVM toolchain

--- buildSrc/src/main/kotlin/io/spine/internal/gradle/kotlin/KotlinConfig.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/gradle/kotlin/KotlinConfig.kt
@@ -26,7 +26,17 @@
 
 package io.spine.internal.gradle.kotlin
 
+import org.gradle.jvm.toolchain.JavaToolchainSpec
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+/**
+ * Sets Java toolchain to the specified version (e.g. "11" or "8").
+ */
+fun KotlinJvmProjectExtension.applyJvmToolchain(version: String) {
+    jvmToolchain {
+        (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(version))
+    }
+}
 
 /**
  * Opts-in to experimental features that we use in our codebase.
