f8cd5d460ba03d6d82b1d5110f1358416e4df19d
tripolkaandrey <andrii.trypilka@nure.ua>
2022-05-10 13:08:44 +0300

    Remove excluding `protoc`-generated source files

--- buildSrc/src/main/kotlin/dokka-for-java.gradle.kts
+++ buildSrc/src/main/kotlin/dokka-for-java.gradle.kts
@@ -26,7 +26,6 @@
 
 import io.spine.internal.dependency.Dokka
 import io.spine.internal.gradle.dokka.onlyJavaSources
-import io.spine.internal.gradle.dokka.onlyNonGeneratedSources
 
 import java.time.LocalDate
 import org.jetbrains.dokka.base.DokkaBase
@@ -61,10 +60,6 @@ tasks.withType<DokkaTask>().configureEach {
     dokkaSourceSets.configureEach {
         sourceRoots.setFrom(
             onlyJavaSources()
-        )
-
-        sourceRoots.setFrom(
-            onlyNonGeneratedSources()
         )
 
         skipEmptyPackages.set(true)
