197c80d16640892a0c910b9ab2129f77f5a69492
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-07-11 12:40:49 +0100

    Bump Kover -> `0.7.2`

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/Kover.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/Kover.kt
@@ -28,7 +28,7 @@ package io.spine.internal.dependency
 
 // https://github.com/Kotlin/kotlinx-kover
 object Kover {
-    const val version = "0.7.0-Beta"
+    const val version = "0.7.2"
     const val id = "org.jetbrains.kotlinx.kover"
     const val classpath = "org.jetbrains.kotlinx:kover-gradle-plugin:$version"
 }
