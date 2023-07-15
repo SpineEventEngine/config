ee5686e8338691c3a35634f0ec30f4cd6284e936
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-07-15 14:44:17 +0100

    Bump ProtoData -> `0.9.4`

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/ProtoData.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/ProtoData.kt
@@ -33,7 +33,7 @@ package io.spine.internal.dependency
  */
 @Suppress("unused", "ConstPropertyName")
 object ProtoData {
-    const val version = "0.9.3"
+    const val version = "0.9.4"
     const val group = "io.spine.protodata"
     const val compiler = "$group:protodata-compiler:$version"
 
