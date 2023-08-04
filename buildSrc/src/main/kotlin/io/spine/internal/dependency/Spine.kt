f86c1e465b07f969c844f52b824cf0316b435656
Yevhenii Nadtochii <yevhenii.nadtochii@teamdev.com>
2023-08-04 11:38:40 +0300

    Bump `base` and `logging`

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
@@ -45,7 +45,7 @@ object Spine {
          *
          * @see <a href="https://github.com/SpineEventEngine/base">spine-base</a>
          */
-        const val base = "2.0.0-SNAPSHOT.185"
+        const val base = "2.0.0-SNAPSHOT.186"
 
         /**
          * The version of [Spine.reflect].
@@ -59,7 +59,7 @@ object Spine {
          *
          * @see <a href="https://github.com/SpineEventEngine/logging">spine-logging</a>
          */
-        const val logging = "2.0.0-SNAPSHOT.197"
+        const val logging = "2.0.0-SNAPSHOT.198"
 
         /**
          * The version of [Spine.testlib].
