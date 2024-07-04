12f7c95f88f40ee8dd53d469b21510e09ba72a2b
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-07-04 22:29:34 +0100

    Bring updates to dependency objects from McJava

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/ProtoData.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/ProtoData.kt
@@ -65,7 +65,7 @@ object ProtoData {
      * The version of ProtoData dependencies.
      */
     val version: String
-    private const val fallbackVersion = "0.30.0"
+    private const val fallbackVersion = "0.50.0"
 
     /**
      * The distinct version of ProtoData used by other build tools.
