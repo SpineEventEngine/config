58345d79798a219e18232ef6375a7d4b8d6762ea
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-10-27 12:10:20 +0000

    Improve code layout

--- buildSrc/src/main/kotlin/io/spine/internal/gradle/protobuf/ProtoTaskExtensions.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/gradle/protobuf/ProtoTaskExtensions.kt
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -262,7 +262,9 @@ private fun GenerateProtoTask.dependOnProcessResourcesTask() {
  * Obtains the name of the `processResource` task for the given source set name.
  */
 private fun processResourceTaskName(sourceSetName: String): String {
-    val infix = if (sourceSetName == "main") "" else sourceSetName.capitalized()
+    val infix =
+        if (sourceSetName == "main") ""
+        else sourceSetName.capitalized()
     return "process${infix}Resources"
 }
 
