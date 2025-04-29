36cde28376b3bfe71ba4b4a8aef5c4556b5ad7bc
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-04-29 20:11:43 +0100

    Fix `kover` dependency type

--- buildSrc/src/main/kotlin/jvm-module.gradle.kts
+++ buildSrc/src/main/kotlin/jvm-module.gradle.kts
@@ -105,7 +105,7 @@ fun Module.configureKotlin(javaVersion: JavaLanguageVersion) {
     // https://github.com/Kotlin/kotlinx-kover?tab=readme-ov-file#to-create-report-combining-coverage-info-from-different-gradle-projects
     // https://github.com/Kotlin/kotlinx-kover/blob/main/kover-gradle-plugin/examples/jvm/merged/build.gradle.kts
     rootProject.dependencies {
-        kover(this)
+        kover(this@configureKotlin)
     }
 
     kover {
