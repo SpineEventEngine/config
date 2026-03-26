78ff7e94534d33944929bebf9f75ea4562828e67
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2026-03-26 17:26:54 +0000

    Bring improvements from Validation

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -192,6 +192,14 @@ dependencies {
     ).forEach {
         implementation(it)
     }
+
+    testImplementation(platform("org.junit:junit-bom:5.11.4"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
 
 dependOnBuildSrcJar()
