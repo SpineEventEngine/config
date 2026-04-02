7392c12d475852485324345cf328e95ed92d445d
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2026-04-02 19:38:23 +0100

    Make `Time` dependency object extend `Dependency`

--- buildSrc/src/main/kotlin/io/spine/dependency/local/Time.kt
+++ buildSrc/src/main/kotlin/io/spine/dependency/local/Time.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025, TeamDev. All rights reserved.
+ * Copyright 2026, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,18 +26,42 @@
 
 package io.spine.dependency.local
 
+import io.spine.dependency.Dependency
+
 /**
  * Spine Time library.
  *
  * @see <a href="https://github.com/SpineEventEngine/time">spine-time</a>
  */
-@Suppress("ConstPropertyName")
-object Time {
-    const val version = "2.0.0-SNAPSHOT.232"
-    const val group = Spine.group
-    const val artifact = "spine-time"
-    const val lib = "$group:$artifact:$version"
-    const val javaExtensions = "$group:$artifact-java:$version"
-    const val kotlinExtensions = "$group:$artifact-kotlin:$version"
-    const val testLib = "${Spine.toolsGroup}:spine-time-testlib:$version"
+@Suppress(
+    "unused" /* Some subprojects do not use all Time artifacts. */,
+    "ConstPropertyName" /* We use custom convention for artifact properties. */,
+    "MemberVisibilityCanBePrivate" /* The properties are used directly by other subprojects. */,
+)
+object Time : Dependency() {
+    override val group = Spine.group
+    override val version = "2.0.0-SNAPSHOT.234"
+    private const val infix = "spine-time"
+
+    fun lib(version: String): String = "$group:$infix:$version"
+    val lib get() = lib(version)
+
+    fun javaExtensions(version: String): String = "$group:$infix-java:$version"
+    val javaExtensions get() = javaExtensions(version)
+
+    fun kotlinExtensions(version: String): String = "$group:$infix-kotlin:$version"
+    val kotlinExtensions get() = kotlinExtensions(version)
+
+    fun testLib(version: String): String = "${Spine.toolsGroup}:time-testlib:$version"
+    val testLib get() = testLib(version)
+
+    override val modules: List<String>
+        get() = listOf(
+            lib,
+            javaExtensions,
+            kotlinExtensions,
+            testLib
+        ).map {
+            it.split(":").let { (g, artifact) -> "$g:$artifact" }
+        }
 }
