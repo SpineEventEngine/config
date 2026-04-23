c29dea7265e30ceac1f164e64d333462429aa5c9
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2026-04-23 18:21:19 +0100

    Bump Logging -> `2.0.0-SNAPSHOT.415`

--- buildSrc/src/main/kotlin/io/spine/dependency/local/Logging.kt
+++ buildSrc/src/main/kotlin/io/spine/dependency/local/Logging.kt
@@ -33,7 +33,7 @@ package io.spine.dependency.local
  */
 @Suppress("ConstPropertyName", "unused")
 object Logging {
-    const val version = "2.0.0-SNAPSHOT.414"
+    const val version = "2.0.0-SNAPSHOT.415"
     const val group = Spine.group
 
     const val loggingArtifact = "spine-logging"
