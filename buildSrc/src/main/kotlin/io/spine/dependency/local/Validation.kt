6097aa121508b49b4e16c470c7045e73b7bad10c
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2026-04-24 21:08:46 +0100

    Bump Validation -> `2.0.0-SNAPSHOT.413`

--- buildSrc/src/main/kotlin/io/spine/dependency/local/Validation.kt
+++ buildSrc/src/main/kotlin/io/spine/dependency/local/Validation.kt
@@ -36,7 +36,7 @@ object Validation {
     /**
      * The version of the Validation library artifacts.
      */
-    const val version = "2.0.0-SNAPSHOT.411"
+    const val version = "2.0.0-SNAPSHOT.413"
 
     /**
      * The last version of Validation compatible with ProtoData.
