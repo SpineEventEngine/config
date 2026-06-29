f2787527b63e31fa58d300005f60d917bc5348c1
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2026-06-29 18:10:52 +0100

    Bump Compiler and Logging

--- buildSrc/src/main/kotlin/io/spine/dependency/local/Compiler.kt
+++ buildSrc/src/main/kotlin/io/spine/dependency/local/Compiler.kt
@@ -27,6 +27,8 @@
 package io.spine.dependency.local
 
 import io.spine.dependency.Dependency
+import io.spine.dependency.local.Compiler.DF_VERSION_ENV
+import io.spine.dependency.local.Compiler.VERSION_ENV
 
 /**
  * Dependencies on the Spine Compiler modules.
@@ -72,7 +74,7 @@ object Compiler : Dependency() {
      * The version of the Compiler dependencies.
      */
     override val version: String
-    private const val fallbackVersion = "2.0.0-SNAPSHOT.056"
+    private const val fallbackVersion = "2.0.0-SNAPSHOT.057"
 
     /**
      * The distinct version of the Compiler used by other build tools.
@@ -81,7 +83,7 @@ object Compiler : Dependency() {
      * transitive dependencies, this is the version used to build the project itself.
      */
     val dogfoodingVersion: String
-    private const val fallbackDfVersion = "2.0.0-SNAPSHOT.056"
+    private const val fallbackDfVersion = "2.0.0-SNAPSHOT.057"
 
     /**
      * The artifact for the Compiler Gradle plugin.
